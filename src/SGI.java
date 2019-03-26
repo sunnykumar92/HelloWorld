57a1fe06071a1dd5e44642b77e2258bb4a358dfd
sunnykumar92 <48937561+sunnykumar92@users.noreply.github.com>
2019-03-26 14:17:15 +1000

    added new class SGI

---
+++ src/SGI.java
@@ -0,0 +1,19 @@
+
+public class SGI {
+
+	String SGI;
+	public SGI()
+	{
+		SGI = "Site Group International";
+		
+	}
+	public String GetSGI()
+	{
+		return SGI;
+	}
+	
+	public void SetSGI(String sgi) {
+		
+		SGI = sgi;
+	}
+}
